760ad073018ee58a0f50cb1669efaaf56785e6c0
James Yarbrough <jjarbrough@gmail.com>
2023-10-01 14:58:40 -0500

    completed exercise one

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Divide(17, 4);
+        }
+        public static int Mod(int a, int b)
+        {
+            return a % b;
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Mult(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            return a / b;
         }
     }
 }
